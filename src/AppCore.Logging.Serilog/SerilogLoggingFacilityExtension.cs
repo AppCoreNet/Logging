9d501f5a4af3226b4110ffebe5d64737703ef3c3
Christian Prochnow <christian.prochnow77@gmail.com>
2019-02-10 18:59:32 +0100

    Add the ability to pass the ILogger when registering the logging extension.

--- src/AppCore.Logging.Serilog/SerilogLoggingFacilityExtension.cs
+++ src/AppCore.Logging.Serilog/SerilogLoggingFacilityExtension.cs
@@ -2,8 +2,11 @@
 // Copyright (c) 2018 the AppCore .NET project.
 
 using AppCore.DependencyInjection.Facilities;
+using AppCore.Diagnostics;
 using AppCore.Logging;
 using AppCore.Logging.Serilog;
+using Serilog;
+using ILogger = Serilog.ILogger;
 
 // ReSharper disable once CheckNamespace
 namespace AppCore.DependencyInjection
@@ -13,9 +16,42 @@ namespace AppCore.DependencyInjection
     /// </summary>
     public class SerilogLoggingFacilityExtension : FacilityExtension<ILoggingFacility>
     {
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SerilogLoggingFacilityExtension"/> class.
+        /// </summary>
+        /// <remarks>Uses </remarks>
+        public SerilogLoggingFacilityExtension()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SerilogLoggingFacilityExtension"/> class.
+        /// </summary>
+        /// <param name="logger">The logger which is used.</param>
+        public SerilogLoggingFacilityExtension(ILogger logger)
+        {
+            Ensure.Arg.NotNull(logger, nameof(logger));
+            _logger = logger;
+        }
+
         /// <inheritdoc />
         protected override void RegisterComponents(IComponentRegistry registry, ILoggingFacility facility)
         {
+            if (_logger == null)
+            {
+                registry.Register<ILogger>()
+                        .Add(c => Log.Logger)
+                        .IfNotRegistered();
+            }
+            else
+            {
+                registry.Register<ILogger>()
+                        .Add(_logger)
+                        .IfNotRegistered();
+            }
+
             registry.Register<ILoggerProvider>()
                     .Add<SerilogLoggerProvider>()
                     .PerContainer()
